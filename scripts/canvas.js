0f5eac915cad240ec14625d91cfcc61d24c373e5
scope <scope@scope-X370.(none)>
2014-10-07 23:00:58 -0400

    Modify colorLine so colors exist in single array
    Putting all the color values in a single array simplifies the sorting
    process.

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -11,23 +11,22 @@ function drawVerticalLine (line) {
 	context.beginPath();
 	context.moveTo(line.pointA.x, line.pointA.y);
 	context.lineTo(line.pointB.x, line.pointB.y);
-	context.strokeStyle = line.color;
+	context.strokeStyle = line.color();
 	context.stroke();
 	return true;
     } else {
 	return false;
     }
-};
+}
 
-function getRandomColor () {
-    var rand = Math.floor(Math.random() * 16777215);
-    return '#' + rand.toString(16);
-};
+function getRandomRGB () {
+    return Math.floor(Math.random() * 16777215);
+}
 
 function drawColorLine (colorLine) {
     for (var i = 0; i < colorLine.length; i++)
 	drawVerticalLine(colorLine[i]);
-};
+}
 
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
--- scripts/colorLine.js
+++ scripts/colorLine.js
@@ -1,4 +1,5 @@
 var colorLine = {
+    valueArr: [],
     getColorLine: function (num) {
 	var x = 50
 	var yA = 10
@@ -8,22 +9,56 @@ var colorLine = {
 	    line = {};
 	    line.pointA = {'x': x, 'y': yA};
 	    line.pointB = {'x': x, 'y': yB};
-	    line.color = getRandomColor();
-	    line.value = parseInt(line.color.slice(1), 16)
+	    line.valueArr = this.valueArr
+	    line.valueIdx = i	    
+	    line.valueArr[i] = getRandomRGB();
+	    line.value = function () {
+		return this.valueArr[this.valueIdx]
+	    };
+	    line.color = function () {
+		return '#' + this.value().toString(16);
+	    };
 	    x += 5;
 	    arr.push(line);
 	}
 	return arr;
     },
-};
+}
+
+// var pillarLine = {
+//     valueArr: [],
+//     getPillarLine: function (num) {
+// 	var x = 50
+// 	var yA = 10
+// 	var yB = 50
+// 	var arr = [];
+// 	for (var i = 0; i < num; i++) {
+// 	    line = {};
+// 	    line.pointA = {'x': x, 'y': yA};
+// 	    line.pointB = {'x': x, 'y': yB};
+// 	    line.valueArr = this.valueArr
+// 	    line.valueIdx = i	    
+// 	    line.valueArr[i] = getRandomRGB();
+// 	    line.value = function () {
+// 		return this.valueArr[this.valueIdx]
+// 	    };
+// 	    line.color = function () {
+// 		return '#' + this.value().toString(16);
+// 	    };
+// 	    x += 5;
+// 	    arr.push(line);
+// 	}
+// 	return arr;
+//     },
+// }
 
 function animateColorLineSorting () {
     partialSort();
     clearCanvas();
-    drawColorLine(colorLine.array);
+    drawColorLine(colorLine.lineArray);
 }
 
-colorLine.array = colorLine.getColorLine(100);
-drawColorLine(colorLine.array);
-partialSort = selectionSort(colorLine.array);
-repeatFromEQ(colorLine.array.length, 1000, animateColorLineSorting);
+colorLine.lineArray = colorLine.getColorLine(100);
+drawColorLine(colorLine.lineArray);
+partialSort = selectionSort(colorLine.valueArr);
+repeatFromEQ(colorLine.valueArr.length, 1000, animateColorLineSorting);
